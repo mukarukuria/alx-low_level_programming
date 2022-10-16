4ac181f7e6537cc972d7090c72a91588fbc8ae72
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-16 09:49:12 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
