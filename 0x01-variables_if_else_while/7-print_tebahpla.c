8149fb0f02c7b728305494b0b2e6223b5ec1baf1
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-16 11:36:04 -0700

    Corrections

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,9 +11,7 @@ int main(void)
 	char i;
 	
 	for (i = 'z'; i >= 'a'; i--)
-    	{
-        	putchar(i);
-    	}
-    	putchar('\n');
-    	return (0);
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
