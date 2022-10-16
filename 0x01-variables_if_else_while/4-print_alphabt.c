bfa886f3007a628383754e236eb751549749cd59
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-16 09:36:31 -0700

    Task 04

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,13 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char n;
 
 	for (n = 'a'; n <= 'z'; n++)
-	{
-		putchar(n);
-	}
+		if (n != 'e' && n != 'q')
+			putchar(n);
 	putchar('\n');
 	return (0);
 }
