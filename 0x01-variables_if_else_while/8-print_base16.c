6b0aea56fb639d83a19470a770bec76a18859200
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-16 10:37:42 -0700

    Commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,8 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int num;
-
-	for (num = 0; num < 10; num++)
-	{
-		putchar((num % 10) + '0');
-	}
-	putchar('\n');
-	return (0);
-}
+	char n;
+	for (n = 0;
