2b6dd124a5a2a2407f8e5c9aca2de2f27ab2a731
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-16 09:02:26 -0700

    Additional Tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return 0;
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main (void){
+	putchar("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+	return 0;
+}
