00ebc51934b44167a4e38d211ea8cd5760991119
mukarukuria <91755512+mukarukuria@users.noreply.github.com>
2022-10-18 22:41:32 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
-/**
- * print_alphabet - Entry point
- * Description: Prints alphabets in small letters with a new line at the end
- */
-
-void print_alphabet(void)
-{
-	char a;
-
-	for (a = 'a'; a <= 'z'; a++)
-		_putchar(a);
-	_putchar('\n');
-}
+/**                                                                                                                                              
+ * print_alphabet_x10 - Entry point                                                                                                              
+ * Description: Prints alphabets in small letters with a new line at the end                                                                     
+ */                                                                                                                                              
+void print_alphabet_x10(void)                                                                                                                    
+{                                                                                                                                                
+        int x = 0;                                                                                                                               
+        while (x < 10)                                                                                                                           
+        {                                                                                                                                        
+                for (char a = 'a'; a <= 'z'; a++)                                                                                                
+                        _putchar(a);                                                                                                             
+                _putchar('\n');                                                                                                                  
+                x++;                                                                                                                             
+        }                                                                                                                                        
+}   
