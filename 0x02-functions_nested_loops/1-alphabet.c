0931b6b17e3e86da88f68028b5f4fb33f72c48b4
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-18 09:37:26 -0700

    Task 01 test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+@include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Prints alphabets in small letters with a new line at the end
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
