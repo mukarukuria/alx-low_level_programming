cd73ef45a181ff196dd6e10dd43bec431013a9d7
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-18 08:37:47 -0700

    functions commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * Description: function to print _putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("\_puchar");
+	return (0)
+}
