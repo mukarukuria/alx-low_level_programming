6350aa613117f761db28deb6c3877e58666ae79e
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-18 09:44:03 -0700

    Task 02

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,12 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	char a:
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
 	return (0);
 }
