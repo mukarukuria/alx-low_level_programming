c4fdce247e720bf414704a5cf64c4b39aa37761b
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-18 08:44:57 -0700

    functions commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <main.h>
 
 /**
  * main - Entry point
@@ -8,6 +8,15 @@
 
 int main(void)
 {
-	printf("\_puchar");
+`
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0)
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-
+Nested loops and functions
