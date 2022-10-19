b5cec3a8682a3a925755909ca0feaf6d019afa1f
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-19 06:28:02 -0700

    Task 03 commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Determines whether a char is lowercase.
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
