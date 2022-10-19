15090d433d6c1f37171b7567de6db7a1e08867d7
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-19 07:36:13 -0700

    Update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,15 @@ int print_sign(int n)
 {
 	if (n > 0)
     	{
-        	putchar('+');
-        	return 1;
+		_putchar('+');
+		return 1;
     	}else if (n == 0)
     	{
-        	putchar('0');
-        	return 0;
+		_putchar('0');
+		return 0;
     	}else
-        	putchar('-');
+	{
+		_putchar('-');
         	return -1;
     	}
 }
