5324a3d7e88e6290ed3b255a017f572d0258e372
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-19 13:50:43 -0700

    Advanced

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 0, fib1 = 0, fib2 = 1, sum;
+
+	while (n < 50)
+	{
+		sum = fib1 + fib2;
+		printf("%d", sum);
+		fib1 = fib2;
+		fib2 = sum;
+		n++;
+		if (n == 50)
+			break;
+		printf(", ");
+	}
+}
