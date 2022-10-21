1edde2849f642d8bb47c3030bf7d2de3e913134f
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-21 14:04:42 -0700

    Update Header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
  * Description: Function declarations of all files in current directory
  * Return: Always 0 (Success)
  */
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
