0e5962113d174f3292549036c9de8a0545d3049d
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-22 00:45:27 -0700

    Advanced Tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - print the integer given
+ * @n: int to be printed out
+ *
+ * Return: void
+ */
+
+void  print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
