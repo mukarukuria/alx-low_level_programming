7291e054aa519a3cc2e3d85a894fa91bce5386ed
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-10-28 02:32:06 -0700

    Final commits

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * rot13- Encodes a string using rot13
+ * @str: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *str)
+{
+	int n = 0, i;
+	char alphabet[52] = { 'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p', 
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[n])
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (str[n] == alphabet[i])
+			{
+				str[n] = rot13key[i];
+				break;
+			}
+		}
+		n++;
+	}
+}
