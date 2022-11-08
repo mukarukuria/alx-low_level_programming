7563ad82678b9d233f441ee8c268d5d56640728d
Timothy Mukaru <mukrutimothy.56@gmail.com>
2022-11-08 05:53:04 -0800

    Advanced tasks

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string splitted into words
+ *
+ * Return: splitted string
+ */
+
+char **strtow(char *str)
+{
+
+}
