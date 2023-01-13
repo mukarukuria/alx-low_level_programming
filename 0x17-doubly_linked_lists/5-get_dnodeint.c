e1b813d7cbddfba09d285e5aaf11266591926727
mukarukuria <timothy.kuria@strathmore.edu>
2023-01-13 10:41:07 +0000

    New files commit

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,4 +11,12 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+
+	return (head);
 }
